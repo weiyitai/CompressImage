0a1d36ce811b8d1e069b538baa9dda1f2ec7b960
weiyitai <qianbajin@gmail.com>
2018-08-27 18:31:22 +0800

    提交呀

--- app/src/main/java/com/qianbajin/compressimage/MainActivity.java
+++ app/src/main/java/com/qianbajin/compressimage/MainActivity.java
@@ -163,7 +163,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static String getReadableFileSize(long size) {
         if (size <= 0) {
             return "0";
-        }
+        }                 
         final String[] units = new String[]{"B", "KB", "MB", "GB", "TB"};
         int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
         return new DecimalFormat("#,##0.#").format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
